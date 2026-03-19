ea187682583c78f4d8b09a9cfc4bc9d565e5cc46
Digensclub <digensclub@LT-SHAH-04-KaliLinux.shah.in>
2026-03-19 11:50:26 +0530

    bugfix: fixing syntax error in main.py

--- apps/main.py
+++ apps/main.py
@@ -19,4 +19,4 @@ async def hello():
     return {"status": "success", "data": message}
 
 if __name__=="__main__":
-    host="127.0.0.1", port=5000, reload=True
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
